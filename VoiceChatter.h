624bae7cca11f0cdf26f6e4fbd4a2329a2e25afe
howe <howehowe@qq.com>
2011-04-28 15:44:55 +0800

    initialized

---
+++ VoiceChatter.h
@@ -0,0 +1,32 @@
+
+// VoiceChatter.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CVoiceChatterApp:
+// 有关此类的实现，请参阅 VoiceChatter.cpp
+//
+
+class CVoiceChatterApp : public CWinAppEx
+{
+public:
+	CVoiceChatterApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVoiceChatterApp theApp;
