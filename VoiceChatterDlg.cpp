897300567435a46789e5f4f48a08dd99634c3443
howe <howehowe@qq.com>
2011-04-29 09:51:15 +0800

    录音

--- VoiceChatterDlg.cpp
+++ VoiceChatterDlg.cpp
@@ -6,10 +6,127 @@
 #include "VoiceChatter.h"
 #include "VoiceChatterDlg.h"
 
+
+#include <InitGuid.h>
+#include <uuids.h>
+
+#include "debug_util.h"
+#include "dshow_enum_helper.h"
+
+
+using boost::intrusive_ptr;
+using std::wstring;
+
+#define DEFAULT_BUFFER_TIME ((float) 0.05)  /* 10 milliseconds*/
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+WinVersion GetWinVersion() {
+    static bool checked_version = false;
+    static WinVersion win_version = WINVERSION_PRE_2000;
+    if (!checked_version) {
+        OSVERSIONINFOEX version_info;
+        version_info.dwOSVersionInfoSize = sizeof version_info;
+        GetVersionEx(reinterpret_cast<OSVERSIONINFO*>(&version_info));
+        if (version_info.dwMajorVersion == 5) {
+            switch (version_info.dwMinorVersion) {
+        case 0:
+            win_version = WINVERSION_2000;
+            break;
+        case 1:
+            win_version = WINVERSION_XP;
+            break;
+        case 2:
+        default:
+            win_version = WINVERSION_SERVER_2003;
+            break;
+            }
+        } else if (version_info.dwMajorVersion == 6) {
+            if (version_info.wProductType != VER_NT_WORKSTATION) {
+                // 2008 is 6.0, and 2008 R2 is 6.1.
+                win_version = WINVERSION_2008;
+            } else {
+                if (version_info.dwMinorVersion == 0) {
+                    win_version = WINVERSION_VISTA;
+                } else {
+                    win_version = WINVERSION_WIN7;
+                }
+            }
+        } else if (version_info.dwMajorVersion > 6) {
+            win_version = WINVERSION_WIN7;
+        }
+        checked_version = true;
+    }
+    return win_version;
+}
+
+HRESULT GetPin( IBaseFilter * pFilter, PIN_DIRECTION dirrequired, int iNum, IPin **ppPin)
+{
+    CComPtr< IEnumPins > pEnum;
+    *ppPin = NULL;
+
+    if (!pFilter)
+        return E_POINTER;
+
+    HRESULT hr = pFilter->EnumPins(&pEnum);
+    if(FAILED(hr)) 
+        return hr;
+
+    ULONG ulFound;
+    IPin *pPin;
+    hr = E_FAIL;
+
+    while(S_OK == pEnum->Next(1, &pPin, &ulFound))
+    {
+        PIN_DIRECTION pindir = (PIN_DIRECTION)3;
+
+        pPin->QueryDirection(&pindir);
+        if(pindir == dirrequired)
+        {
+            if(iNum == 0)
+            {
+                *ppPin = pPin;  // Return the pin's interface
+                hr = S_OK;      // Found requested pin, so clear error
+                break;
+            }
+            iNum--;
+        } 
+
+        pPin->Release();
+    } 
+
+    return hr;
+}
+
+void UtilDeleteMediaType(AM_MEDIA_TYPE *pmt)
+{
+    // Allow NULL pointers for coding simplicity
+    if (pmt == NULL) {
+        return;
+    }
+
+    // Free media type's format data
+    if (pmt->cbFormat != 0) 
+    {
+        CoTaskMemFree((PVOID)pmt->pbFormat);
+
+        // Strictly unnecessary but tidier
+        pmt->cbFormat = 0;
+        pmt->pbFormat = NULL;
+    }
+
+    // Release interface
+    if (pmt->pUnk != NULL) 
+    {
+        pmt->pUnk->Release();
+        pmt->pUnk = NULL;
+    }
+
+    // Free media type
+    CoTaskMemFree((PVOID)pmt);
+}
 
 // 用于应用程序“关于”菜单项的 CAboutDlg 对话框
 
@@ -55,7 +172,9 @@ CVoiceChatterDlg::CVoiceChatterDlg(CWnd* pParent /*=NULL*/)
 
 void CVoiceChatterDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
+    CDialog::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_COMBO1, m_microphoneCombox);
+    DDX_Control(pDX, IDC_COMBO2, m_microphonePinCombox);
 }
 
 BEGIN_MESSAGE_MAP(CVoiceChatterDlg, CDialog)
@@ -63,6 +182,9 @@ BEGIN_MESSAGE_MAP(CVoiceChatterDlg, CDialog)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
 	//}}AFX_MSG_MAP
+    ON_BN_CLICKED(IDOK, &CVoiceChatterDlg::OnBnClickedOk)
+    ON_BN_CLICKED(IDC_BUTTON1, &CVoiceChatterDlg::OnBnClickedButton1)
+    ON_BN_CLICKED(IDC_BUTTON2, &CVoiceChatterDlg::OnBnClickedButton2)
 END_MESSAGE_MAP()
 
 
@@ -98,6 +220,48 @@ BOOL CVoiceChatterDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+    CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+    m_captureFilter = -1;
+    m_capturePin = -1;
+
+    HRESULT r = initCapture();
+    if (FAILED(r))
+        PostQuitMessage(0);
+
+    initGraph();
+
+
+    if (!checkCapture())
+    {
+        m_microphoneCombox.ShowWindow(SW_SHOW);
+        m_microphonePinCombox.ShowWindow(SW_SHOW);
+
+        m_microphoneCombox.ResetContent();
+        for (int i = 0; i < m_captureFilterVec.size(); i++)
+        {
+            m_microphoneCombox.AddString(m_captureFilterVec[i].Name.c_str());
+        }
+        m_microphoneCombox.SetCurSel(0);
+
+        m_microphonePinCombox.ResetContent();
+
+        for (int i = 0; i < m_captureFilterVec[0].PinVec.size(); i++)
+        {
+            m_microphonePinCombox.AddString(m_captureFilterVec[0].PinVec[i].c_str());
+        }
+
+        m_microphonePinCombox.SetCurSel(0);
+
+        m_captureFilter = 0;
+        m_capturePin = 0;
+    }
+    else
+    {
+        if (!buildMicrophoneGraph())
+            PostQuitMessage(0);   
+    }
+
+
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -151,3 +315,509 @@ HCURSOR CVoiceChatterDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+HRESULT CVoiceChatterDlg::initGraph()
+{
+    HRESULT r;
+    r = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER, 
+        IID_IGraphBuilder, (void**)&m_graphBuilder);
+
+    if (FAILED(r) || !m_graphBuilder)
+        return E_NOINTERFACE;
+
+    // Get useful interfaces from the graph builder
+    r = m_graphBuilder->QueryInterface(IID_IMediaControl,
+        (void **)&m_mediaControl);
+
+    if (FAILED(r))
+        return r;
+
+    return r;
+}
+
+HRESULT CVoiceChatterDlg::initCapture()
+{
+    int mixerNum = 0;
+    int microphoneNum = 0;
+    intrusive_ptr<ICreateDevEnum> sysDevEnum = NULL;
+    HRESULT r = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, 
+        CLSCTX_INPROC, IID_ICreateDevEnum, 
+        (void **)&sysDevEnum);
+
+    if FAILED(r)
+        return r;
+
+    intrusive_ptr<IEnumMoniker> pEnumCat = NULL;
+    r = sysDevEnum->CreateClassEnumerator(CLSID_AudioInputDeviceCategory, (IEnumMoniker**)&pEnumCat, 0);
+
+    if (SUCCEEDED(r))
+    {
+        // Enumerate all filters using the category enumerator
+        r = enumFiltersAndMonikersToList(pEnumCat.get());
+
+    }
+
+
+    intrusive_ptr<IMoniker> pMoniker = NULL;
+    intrusive_ptr<IBaseFilter> filter;
+    for (int i = 0;i < m_captureFilterVec.size(); i++)
+    {
+        pMoniker = m_captureFilterVec[i].Moniker;
+        // Use the moniker to create the specified audio capture device
+        r = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&filter);   
+        if (FAILED(r))
+            return r;
+
+        r = enumPinsOnFilter(filter.get(), PINDIR_INPUT, i);
+        if (FAILED(r))
+            return r;
+
+        //filter->Release();
+    }
+
+    return S_OK;
+
+}
+
+HRESULT CVoiceChatterDlg::enumFiltersAndMonikersToList( IEnumMoniker *pEnumCat )
+{
+    HRESULT r=S_OK;
+    IMoniker* pMoniker=0;
+    ULONG cFetched=0;
+    VARIANT varName={0};
+    int nFilters=0;
+
+    // If there are no filters of a requested type, show default string
+    if (!pEnumCat)
+    {
+        return S_FALSE;
+    }
+
+    // Enumerate all items associated with the moniker
+    while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+    {
+        intrusive_ptr<IPropertyBag> pPropBag;
+        ASSERT(pMoniker);
+
+        // Associate moniker with a file
+        r = pMoniker->BindToStorage(0, 0, IID_IPropertyBag, 
+            (void **)&pPropBag);
+        ASSERT(SUCCEEDED(r));
+        ASSERT(pPropBag);
+        if (FAILED(r))
+            continue;
+
+        // Read filter name from property bag
+        varName.vt = VT_BSTR;
+        r = pPropBag->Read(L"FriendlyName", &varName, 0);
+        if (FAILED(r))
+            continue;
+
+        // Get filter name (converting BSTR name to a CString)
+        CString str(varName.bstrVal);
+        SysFreeString(varName.bstrVal);
+        nFilters++;
+
+        TCaptureFilter filter = {str, pMoniker};
+        m_captureFilterVec.push_back(filter);
+
+        // Cleanup interfaces
+        //SAFE_RELEASE(pPropBag);
+
+        // Intentionally DO NOT release the pMoniker, since it is
+        // stored in a listbox for later use
+    }
+
+    return r;
+}
+
+HRESULT CVoiceChatterDlg::enumPinsOnFilter(IBaseFilter *pFilter, PIN_DIRECTION PinDir, int index)
+{
+    HRESULT r;
+    intrusive_ptr<IEnumPins> pEnum = NULL;
+    intrusive_ptr<IPin> pPin = NULL;
+
+    // Verify filter interface
+    if (!pFilter)
+        return E_NOINTERFACE;
+
+    // Get pin enumerator
+    r = pFilter->EnumPins((IEnumPins **)&pEnum);
+    if (FAILED(r))
+        return r;
+
+    pEnum->Reset();
+
+    // Enumerate all pins on this filter
+    while((r = pEnum->Next(1, (IPin **)&pPin, 0)) == S_OK)
+    {
+        PIN_DIRECTION PinDirThis;
+
+        r = pPin->QueryDirection(&PinDirThis);
+        if (FAILED(r))
+        {
+            //pPin->Release();
+            continue;
+        }
+
+        // Does the pin's direction match the requested direction?
+        if (PinDir == PinDirThis)
+        {
+            PIN_INFO pininfo={0};
+
+            // Direction matches, so add pin name to listbox
+            r = pPin->QueryPinInfo(&pininfo);
+            if (SUCCEEDED(r))
+            {
+                wstring str = pininfo.achName;
+                m_captureFilterVec[index].PinVec.push_back(str);
+            }
+
+            // The pininfo structure contains a reference to an IBaseFilter,
+            // so you must release its reference to prevent resource a leak.
+            pininfo.pFilter->Release();
+        }
+        //pPin->Release();
+    }
+    //pEnum->Release();
+
+    return r;
+}
+void CVoiceChatterDlg::OnBnClickedOk()
+{
+    // TODO: 在此添加控件通知处理程序代码
+    m_mediaControl->Stop();
+    CoUninitialize();
+    OnOK();
+}
+
+bool CVoiceChatterDlg::buildMicrophoneGraph( int filterIndex, int pinIndex )
+{
+    HRESULT r;
+    int index = -1;
+
+    if (filterIndex != -1)
+    {
+        index = findFilter((IBaseFilter**)&m_pInputDevice, NULL, filterIndex);
+    }
+    else
+    {
+        if (GetWinVersion() >= WINVERSION_VISTA)
+            index = findFilter((IBaseFilter **)&m_pInputDevice, L"麦克风");
+        else
+            index = findFilter((IBaseFilter **)&m_pInputDevice);
+    }
+
+
+    if (index == -1)
+        return false;
+
+    if (pinIndex != -1)
+    {
+        activePin(m_pInputDevice.get(), index, NULL, pinIndex);
+    }
+    else
+    {
+        if (GetWinVersion() >= WINVERSION_VISTA)
+            activePin(m_pInputDevice.get(), index);
+        else
+        {
+            activePin(m_pInputDevice.get(), index, L"麦克风");
+        }
+    }
+
+
+    SetAudioProperties(m_pInputDevice.get());
+
+    r = m_graphBuilder->AddFilter(m_pInputDevice.get(), L"Microphone Capture");
+    if (FAILED(r))
+        return false;
+
+    intrusive_ptr<IBaseFilter> renderer;
+
+    r = CoCreateInstance(CLSID_AudioRender, 
+        NULL, CLSCTX_INPROC_SERVER,IID_IBaseFilter,
+        reinterpret_cast<void**>(&renderer));
+
+    if (FAILED(r))
+        return false;
+
+    intrusive_ptr<IPin> sourceOutPin;
+    r = GetPinByDirection(m_pInputDevice.get(), 
+        reinterpret_cast<IPin**>(&sourceOutPin), 
+        PINDIR_OUTPUT);
+
+    if (FAILED(r))
+        return false;
+
+    r = m_graphBuilder->AddFilter((IBaseFilter *)renderer.get(), L"DShow Render");
+    if (FAILED(r))
+        return false;
+
+    intrusive_ptr<IPin> rendererInputPin;
+    r = GetPinByDirection(renderer.get(), 
+        reinterpret_cast<IPin**>(&rendererInputPin), 
+        PINDIR_INPUT);
+
+    if (FAILED(r))
+        return false;
+
+
+    r = m_graphBuilder->ConnectDirect(sourceOutPin.get(), rendererInputPin.get(), NULL);
+    if (FAILED(r))
+        return false;  
+
+    return true;
+}
+
+int CVoiceChatterDlg::findFilter( IBaseFilter** filter, wchar_t* str , int index )
+{
+    int r = -1;
+
+    if (index != -1)
+    {
+        IMoniker *pMoniker = m_captureFilterVec[index].Moniker;
+        HRESULT hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)filter);
+        if (FAILED(hr))
+            return -1;
+
+        return index;
+    }
+
+    if (str == NULL)
+    {
+        IMoniker *pMoniker = m_captureFilterVec[0].Moniker;
+        HRESULT hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)filter);
+        if (FAILED(hr))
+            return -1;
+
+        return 0;
+    }
+
+    for (int i = 0; i < m_captureFilterVec.size(); i++)
+    {
+        if (m_captureFilterVec[i].Name.find(str) != wstring::npos)
+        {
+            IMoniker *pMoniker = m_captureFilterVec[i].Moniker;
+            HRESULT hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)filter);
+            if (FAILED(hr))
+                return -1;
+
+            return i;
+        }
+
+    }       
+
+    return -1;
+}
+
+bool CVoiceChatterDlg::checkCapture()
+{
+    int mixCount = 0;
+    int captureCount = 0;
+
+    for (int i = 0; i < m_captureFilterVec.size(); i++)
+    {
+        if (GetWinVersion() >= WINVERSION_VISTA)
+        {
+            if (m_captureFilterVec[i].Name.find(L"麦克风") != wstring::npos || m_captureFilterVec[i].Name.find(L"Mic") != wstring::npos)
+            {
+                captureCount++;
+            }
+
+            if (m_captureFilterVec[i].Name.find(L"混音") != wstring::npos || m_captureFilterVec[i].Name.find(L"Mix") != wstring::npos)
+            {
+                mixCount++;
+            }
+        }
+        else
+        {
+            for (int j = 0; j < m_captureFilterVec[i].PinVec.size(); j++)
+            {
+                if (m_captureFilterVec[i].PinVec[j].find(L"麦克风") != wstring::npos || m_captureFilterVec[i].PinVec[j].find(L"Mic") != wstring::npos)
+                {
+                    captureCount++;
+                }
+
+                if (m_captureFilterVec[i].PinVec[j].find(L"混音") != wstring::npos || m_captureFilterVec[i].PinVec[j].find(L"Mix") != wstring::npos)
+                {
+                    mixCount++;
+                }
+            }
+        }
+
+    }
+
+    if (captureCount != 1 || mixCount != 1)
+        return false;
+
+    return true;
+}
+
+HRESULT CVoiceChatterDlg::activePin( IBaseFilter* filter, int index, wchar_t* str /*= NULL*/, int pinIndex /*= -1*/ )
+{
+    HRESULT r = -1;
+    int nActivePin = -1;
+
+    if (pinIndex == -1)
+    {
+        if (GetWinVersion() >= WINVERSION_VISTA)
+        {
+            nActivePin = 0;
+        }
+        else
+        {
+            for (int j = 0; j < m_captureFilterVec[index].PinVec.size(); j++)
+            {
+                if (m_captureFilterVec[index].PinVec[j].find(str) != wstring::npos)
+                {
+                    nActivePin = j;
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        nActivePin = pinIndex;
+    }
+
+    intrusive_ptr<IPin> pPin=0;
+    intrusive_ptr<IAMAudioInputMixer> pPinMixer;
+
+    // How many pins are in the input pin list?
+    int nPins = m_captureFilterVec[index].PinVec.size();
+
+
+    // Activate the selected input pin and deactivate all others
+    for (int i=0; i<nPins; i++)
+    {
+        // Get this pin's interface
+        r = GetPin(filter, PINDIR_INPUT, i, (IPin **)&pPin);
+        if (SUCCEEDED(r))
+        {
+            r = pPin->QueryInterface(IID_IAMAudioInputMixer, (void **)&pPinMixer);
+            if (SUCCEEDED(r))
+            {
+                // If this is our selected pin, enable it
+                if (i == nActivePin)
+                {
+                    // Set any other audio properties on this pin
+                    //r = SetInputPinProperties(pPinMixer);
+
+                    // If there is only one input pin, this method
+                    // might return E_NOTIMPL.
+                    r = pPinMixer->put_Enable(TRUE);
+                }
+                // Otherwise, disable it
+                else
+                {
+                    //r = pPinMixer->put_Enable(FALSE);
+                }
+
+               // pPinMixer->Release();
+            }
+
+            // Release pin interfaces
+           // pPin->Release();
+        }
+    }
+
+    return S_OK;
+}
+
+HRESULT CVoiceChatterDlg::SetAudioProperties( IBaseFilter* filter )
+{
+    HRESULT hr=0;
+    IPin *pPin=0;
+    IAMBufferNegotiation *pNeg=0;
+    IAMStreamConfig *pCfg=0;
+    int nFrequency=8000;
+    int nChannels = 2;
+    int nBytesPerSample = 2;
+    // Determine audio properties
+    //     int nChannels = IsDlgButtonChecked(IDC_RADIO_MONO) ? 1 : 2;
+    //     int nBytesPerSample = IsDlgButtonChecked(IDC_RADIO_8) ? 1 : 2;
+
+    //     // Determine requested frequency
+    //     if (IsDlgButtonChecked(IDC_RADIO_11KHZ))      
+    //         nFrequency = 11025;
+    //     else if (IsDlgButtonChecked(IDC_RADIO_22KHZ)) 
+    //         nFrequency = 22050;
+    //     else 
+    //         nFrequency = 44100;
+
+    // Find number of bytes in one second
+    long lBytesPerSecond = (long) (nBytesPerSample * nFrequency * nChannels);
+
+    // Set to 50ms worth of data    
+    long lBufferSize = (long) ((float) lBytesPerSecond * DEFAULT_BUFFER_TIME);
+
+    for (int i=0; i<2; i++)
+    {
+        hr = GetPin(filter, PINDIR_OUTPUT, i, &pPin);
+        if (SUCCEEDED(hr))
+        {
+            // Get buffer negotiation interface
+            hr = pPin->QueryInterface(IID_IAMBufferNegotiation, (void **)&pNeg);
+            if (FAILED(hr))
+            {
+                pPin->Release();
+                return hr;
+            }
+
+            // Set the buffer size based on selected settings
+            ALLOCATOR_PROPERTIES prop={0};
+            prop.cbBuffer = lBufferSize;
+            prop.cBuffers = 6;
+            prop.cbAlign = nBytesPerSample * nChannels;
+            hr = pNeg->SuggestAllocatorProperties(&prop);
+            pNeg->Release();
+
+            // Now set the actual format of the audio data
+            hr = pPin->QueryInterface(IID_IAMStreamConfig, (void **)&pCfg);
+            if (FAILED(hr))
+            {
+                pPin->Release();
+                return hr;
+            }            
+
+            // Read current media type/format
+            AM_MEDIA_TYPE *pmt={0};
+            hr = pCfg->GetFormat(&pmt);
+
+            if (SUCCEEDED(hr))
+            {
+                // Fill in values for the new format
+                WAVEFORMATEX *pWF = (WAVEFORMATEX *) pmt->pbFormat;
+                pWF->nChannels = (WORD) nChannels;
+                pWF->nSamplesPerSec = nFrequency;
+                pWF->nAvgBytesPerSec = lBytesPerSecond;
+                pWF->wBitsPerSample = (WORD) (nBytesPerSample * 8);
+                pWF->nBlockAlign = (WORD) (nBytesPerSample * nChannels);
+
+                // Set the new formattype for the output pin
+                hr = pCfg->SetFormat(pmt);
+                UtilDeleteMediaType(pmt);
+            }
+
+            // Release interfaces
+            pCfg->Release();
+            pPin->Release();
+        }
+        else
+            break;
+    }
+    // No more output pins on this filter
+    return hr;
+}
+void CVoiceChatterDlg::OnBnClickedButton1()
+{
+    // TODO: 在此添加控件通知处理程序代码
+    m_mediaControl->Run();
+}
+
+void CVoiceChatterDlg::OnBnClickedButton2()
+{
+    // TODO: 在此添加控件通知处理程序代码
+    m_mediaControl->Pause();
+}
--- VoiceChatterDlg.h
+++ VoiceChatterDlg.h
@@ -4,6 +4,30 @@
 
 #pragma once
 
+#include <string>
+#include <vector>
+#include <strmif.h>
+#include <DShow.h>
+#include <MMSystem.h>
+#include "afxwin.h"
+
+#include "intrusive_ptr_helper.h"
+
+struct IGraphBuilder;
+struct IBaseFilter;
+struct IMoniker;
+struct IMediaControl;
+
+enum WinVersion {
+    WINVERSION_PRE_2000 = 0,  // Not supported
+    WINVERSION_2000 = 1,
+    WINVERSION_XP = 2,
+    WINVERSION_SERVER_2003 = 3,
+    WINVERSION_VISTA = 4,
+    WINVERSION_2008 = 5,
+    WINVERSION_WIN7 = 6,
+};
+
 
 // CVoiceChatterDlg ¶Ô»°¿ò
 class CVoiceChatterDlg : public CDialog
@@ -29,4 +53,39 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+
+public:
+    CComboBox m_microphoneCombox;
+    CComboBox m_microphonePinCombox;
+
+private:
+    struct TCaptureFilter
+    {
+        std::wstring Name;
+        IMoniker* Moniker;
+        std::vector<std::wstring> PinVec; 
+    };
+    std::vector<TCaptureFilter> m_captureFilterVec;
+
+    boost::intrusive_ptr<IGraphBuilder> m_graphBuilder;
+    boost::intrusive_ptr<IMediaControl> m_mediaControl;
+    boost::intrusive_ptr<IBaseFilter> m_pInputDevice;
+
+    int m_captureFilter;
+    int m_capturePin;
+
+    HRESULT initGraph();
+    HRESULT initCapture();
+    HRESULT enumFiltersAndMonikersToList(IEnumMoniker *pEnumCat);
+    HRESULT enumPinsOnFilter (IBaseFilter *pFilter, PIN_DIRECTION PinDir, int index);
+    bool buildMicrophoneGraph(int filterIndex = -1, int pinIndex = -1);
+    int findFilter(IBaseFilter** filter, wchar_t* str = NULL, int index = -1);
+    bool checkCapture(); 
+    HRESULT activePin(IBaseFilter* filter, int index, wchar_t* str = NULL, int pinIndex = -1);
+    HRESULT SetAudioProperties(IBaseFilter* filter);
+
+public:
+    afx_msg void OnBnClickedOk();
+    afx_msg void OnBnClickedButton1();
+    afx_msg void OnBnClickedButton2();
 };
